4587f51f7aab8d9505e9f583b25bf0132f5de63e
Shokhw7 <sniyatkabilov2009@gmail.com>
2025-08-24 18:37:01 +0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
-import React, { useState } from "react";
+import React, { useState,  useEffect} from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addTodo, editTodo, removeTodo } from "./app/features/todoSlice";
+import {
+  addTodo,
+  editTodo,
+  removeTodo,
+  filter,
+  reorder,
+  clearCompleted,
+} from "./app/features/todoSlice";
+import { DragDropContext, Droppable, Draggable } from "@hello-pangea/dnd";
 import "./app.css";
-
+import { motion, AnimatePresence } from "framer-motion";
 
 function App() {
   const [id, setId] = useState("");
@@ -10,7 +18,13 @@ function App() {
   const [completed, setCompleted] = useState(false);
 
   const todos = useSelector((state) => state.todos.todos);
+  const itemsLeft = todos.filter((todo) => !todo.completed).length;
+  const [theme, setTheme] = useState("light");
   const dispatch = useDispatch();
+
+   useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -28,7 +42,7 @@ function App() {
       } else {
         dispatch(
           addTodo({
-            id: Math.random(),
+            id: Math.random().toString(36).substr(2, 9),
             title: title.trim(),
             completed,
           })
@@ -40,55 +54,144 @@ function App() {
     }
   };
 
-  const handleEdit = (todo) => {
-    setTitle(todo.title);
-    setCompleted(todo.completed);
-    setId(todo.id);
-  };
-
-  // const handleDelete = (id) => {
-  //   dispatch(removeTodo(id));
+  // const handleEdit = (todo) => {
+  //   setTitle(todo.title);
+  //   setCompleted(todo.completed);
+  //   setId(todo.id);
   // };
 
-  return (
-    <div>
-      <h1 className="crud">Todo List Crud</h1>
-      <form className="todo-form" onSubmit={handleSubmit}>
-        <input className="title-inp"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          placeholder="Title"
-        />
+  const handleDragEnd = (result) => {
+    if (!result.destination) return;
 
+    const reordered = Array.from(todos);
+    const [moved] = reordered.splice(result.source.index, 1);
+    reordered.splice(result.destination.index, 0, moved);
+
+    dispatch(reorder(reordered));
+  };
+
+  return (
+    <div className={`container ${theme}`}>
+      <h1 className="header">
+        TODO
+        <button
+          className="theme-toggle"
+          onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+        >
+          <img
+            src={theme === "light" ? "./images/moon.svg" : "./images/sun.svg"}
+          />
+        </button>
+      </h1>
+      <div className="create-inp">
         <label className="completed">
           <input
             type="checkbox"
             checked={completed}
             onChange={(e) => setCompleted(e.target.checked)}
           />
-          Completed
+          <span className="checkmark">
+            <img src="./images/check.svg" alt="" />
+          </span>
         </label>
+        <input
+          className="title-inp"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Create a new todo..."
+        />
+      </div>
 
-        <button className="submit-btn" type="submit">{id ? "Update" : "Add"} Todo</button>
+      <form className="todo-form" onSubmit={handleSubmit}>
+        <button className="submit-btn" type="submit">
+          Add Todo
+        </button>
       </form>
-      <div>
-        <ul className="todo-list">
-          {todos &&
-            todos.map((todo) => {
-              return (
-                <li className="todo-item" key={todo.id}>
-                  <h4 className="todo-title" style={{ opacity: todo.completed ? "0.5" : "1" }}>
-                    {todo.title}
-                    <button className="todo-delete" onClick={() => dispatch(removeTodo(todo.id))}>
-                      Delete
-                    </button>
-                    <button className="todo-edit" onClick={() => handleEdit(todo)}>Edit Todo</button>
-                  </h4>
-                </li>
-              );
-            })}
-        </ul>
-      </div>{" "}
+
+      <DragDropContext onDragEnd={handleDragEnd}>
+        <Droppable droppableId="todos">
+          {(provided) => (
+            <ul
+              className="todo-list"
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
+              <AnimatePresence className="todo-cards">
+                {todos &&
+                  todos.map((todo, index) => (
+                    <Draggable
+                      key={todo.id}
+                      draggableId={todo.id.toString()}
+                      index={index}
+                    >
+                      {(provided) => (
+                        <motion.li
+                          className="todo-item"
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          layout
+                          initial={{ opacity: 0, y: 30 }}
+                          animate={{ opacity: 1, y: 0 }}
+                          exit={{ opacity: 0, scale: 0.8 }}
+                          transition={{ duration: 0.3 }}
+                        >
+                          <label className="completed">
+                            <input
+                              type="checkbox"
+                              checked={completed}
+                              onChange={(e) => setCompleted(e.target.checked)}
+                            />
+                            <span className="checkmark">
+                              <img
+                                src="./images/check.svg"
+                                alt=""
+                                className=""
+                              />
+                            </span>
+                          </label>
+                          <h4
+                            className="todo-title"
+                            style={{ opacity: todo.completed ? "0.5" : "1" }}
+                          >
+                            {todo.title}
+                            <button
+                              className="todo-delete"
+                              onClick={() => dispatch(removeTodo(todo.id))}
+                            >
+                              <img src="./images/cross.svg" alt="" />
+                            </button>
+                            {/* <button
+                                className="todo-edit"
+                                onClick={() => handleEdit(todo)}
+                              >
+                                Edit Todo
+                              </button> */}
+                          </h4>
+                        </motion.li>
+                      )}
+                    </Draggable>
+                  ))}
+              </AnimatePresence>
+              {provided.placeholder}
+              <div className="filters">
+                <span className="items-left">{itemsLeft} items left</span>
+
+                <button onClick={() => dispatch(filter("all"))}>All</button>
+                <button onClick={() => dispatch(filter("completed"))}>
+                  Completed
+                </button>
+                <button onClick={() => dispatch(filter("active"))}>
+                  Active
+                </button>
+                <button onClick={() => dispatch(clearCompleted())}>
+                  Clear Completed
+                </button>
+              </div>
+            </ul>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 }
--- src/app/features/todoSlice.js
+++ src/app/features/todoSlice.js
@@ -1,33 +1,51 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   todos: [],
+  backup: [],
 };
 
 const todosSlice = createSlice({
-  name: 'todos',
+  name: "todos",
   initialState,
   reducers: {
     addTodo: (state, { payload }) => {
       state.todos.push(payload);
+      state.backup = [...state.todos];
     },
     removeTodo: (state, { payload }) => {
       state.todos = state.todos.filter((todo) => todo.id !== payload);
+      state.backup = [...state.todos];
     },
     editTodo: (state, { payload }) => {
-      state.todos = state.todos.map((todo) => {
-        if (todo.id === payload.id) {
-          return {
-            ...todo,
-            title: payload.title,
-            completed: payload.completed,
-          };
-        }
-        return todo;
-      });
+      state.todos = state.todos.map((todo) =>
+        todo.id === payload.id
+          ? { ...todo, title: payload.title, completed: payload.completed }
+          : todo
+      );
+      state.backup = [...state.todos];
+    },
+    clearCompleted: (state) => {
+      state.todos = state.todos.filter((todo) => !todo.completed);
+      state.backup = [...state.todos];
+    },
+
+    filter: (state, { payload }) => {
+      if (payload === "all") {
+        state.todos = state.backup;
+      } else if (payload === "completed") {
+        state.todos = state.backup.filter((item) => item.completed);
+      } else if (payload === "active") {
+        state.todos = state.backup.filter((item) => !item.completed);
+      }
+    },
+    reorder: (state, { payload }) => {
+      state.todos = payload;
+      state.backup = [...payload];
     },
   },
 });
 
-export const { addTodo, removeTodo, editTodo } = todosSlice.actions;
+export const { addTodo, removeTodo, editTodo, filter, reorder, clearCompleted } =
+  todosSlice.actions;
 export default todosSlice.reducer;
